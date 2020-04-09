ff5b4e5bb8c7da50d8777a34bc44e9dd945ce0e4
zhangshuaihua <zhangshuaihua@etonetech.com>
2020-04-09 11:48:46 +0800

    角色管理功能提交

--- src/main/java/com/etone/protocolsupply/controller/systemControl/RoleController.java
+++ src/main/java/com/etone/protocolsupply/controller/systemControl/RoleController.java
@@ -5,7 +5,9 @@ import com.etone.protocolsupply.controller.GenericController;
 import com.etone.protocolsupply.model.dto.ResponseValue;
 import com.etone.protocolsupply.model.dto.systemControl.RoleCollectionDto;
 import com.etone.protocolsupply.model.dto.systemControl.RoleDto;
+import com.etone.protocolsupply.model.entity.user.Permissions;
 import com.etone.protocolsupply.model.entity.user.Role;
+import com.etone.protocolsupply.service.system.PermissionService;
 import com.etone.protocolsupply.service.system.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,6 +20,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "${jwt.route.path}/role")
@@ -25,6 +29,9 @@ public class RoleController extends GenericController {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private PermissionService permissionService;
 
 
     /**
@@ -74,6 +81,114 @@ public class RoleController extends GenericController {
         responseBuilder.message("保存角色成功");
         return responseBuilder.build();
     }
+
+
+    /**
+     * 根据角色id删除角色
+     * @param roleId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{roleId}",
+            method = RequestMethod.DELETE,
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseValue deleteUser(@PathVariable("roleId") String roleId) {
+        ResponseValue.ResponseBuilder responseBuilder = ResponseValue.createBuilder();
+        roleService.delete(Long.parseLong(roleId));
+        responseBuilder.message("删除角色成功");
+        return responseBuilder.build();
+    }
+
+    /**
+     * 根据角色id查询该角色所拥有的权限(一级二级权限)
+     * @param roleId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/getRolePermission/{roleId}",
+            method = RequestMethod.GET,
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    public ResponseValue getPermissionByRoleId(@PathVariable("roleId") String roleId) {
+        ResponseValue.ResponseBuilder responseBuilder = ResponseValue.createBuilder();
+        List<Map<String,Object>> permissions = permissionService.getPermissionByRoleId(Long.parseLong(roleId));
+        responseBuilder.data(permissions);
+        return responseBuilder.build();
+    }
+
+    /**
+     * 根据角色id和二级权限id查询该角色所拥有的三级权限
+     * @param roleId  角色id
+     * @param secondPermissionId  二级权限id
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/getThirdPermissionByRoleId/{roleId}/{secondPermissionId}",
+            method = RequestMethod.GET,
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    public ResponseValue getThirdPermissionByRoleId(@PathVariable("roleId") String roleId,
+                                                    @PathVariable("secondPermissionId") String secondPermissionId) {
+        ResponseValue.ResponseBuilder responseBuilder = ResponseValue.createBuilder();
+        List<Permissions> permissions = permissionService.getThirdPermissionByRoleId(roleId,secondPermissionId);
+        responseBuilder.data(permissions);
+        return responseBuilder.build();
+    }
+
+    /**
+     * 查询所有一二级权限菜单
+     */
+    @ResponseBody
+    @RequestMapping(value = "/getAllPermission",
+            method = RequestMethod.GET,
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    public ResponseValue getAllPermission() {
+        ResponseValue.ResponseBuilder responseBuilder = ResponseValue.createBuilder();
+        List<Map<String,Object>> permissions =  permissionService.getAllPermission();
+        responseBuilder.data(permissions);
+        return responseBuilder.build();
+    }
+
+
+    /**
+     * 根据二级权限id查询其对应的所有三级权限
+     */
+    @ResponseBody
+    @RequestMapping(value = "/getThirdPermissionBySecondPermissionId/{permissionId}",
+            method = RequestMethod.GET,
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    public ResponseValue getThirdPermissionBySecondPermissionId(@PathVariable("permissionId") String permissionId) {
+        ResponseValue.ResponseBuilder responseBuilder = ResponseValue.createBuilder();
+        List<Permissions> permissions =  permissionService.getThirdPermissionBySecondPermissionId(permissionId);
+        responseBuilder.data(permissions);
+        return responseBuilder.build();
+    }
+
+
+    /**
+     * 编辑角色对应的权限
+     * @param
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(
+            value = "/updateRolePermissions",
+            method = RequestMethod.POST,
+            consumes = {"application/json"},
+            produces = {"application/json"})
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseValue updateRolePermissions(@Validated
+                                    @RequestBody RoleDto roleDto) {
+        ResponseValue.ResponseBuilder responseBuilder = ResponseValue.createBuilder();
+        roleService.updateRolePermissions(roleDto);
+        responseBuilder.message("更新角色权限成功");
+        return responseBuilder.build();
+    }
+
 
 
 }
---
+++ src/main/java/com/etone/protocolsupply/service/system/PermissionService.java
@@ -0,0 +1,166 @@
+package com.etone.protocolsupply.service.system;
+
+
+import com.etone.protocolsupply.model.entity.user.Permissions;
+import com.etone.protocolsupply.repository.PermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Transactional(rollbackFor = Exception.class)
+@Service
+public class PermissionService {
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    public List<Map<String,Object>> getAllPermission() {
+        //所有权限
+        List<Permissions> permissionsList = permissionRepository.findAll();
+        //要返回的结果集
+        List<Map<String,Object>> returnMap = new ArrayList<>();
+
+        //处理数据
+        proPermissions(permissionsList, returnMap);
+
+        return returnMap;
+    }
+
+    private void proPermissions(List<Permissions> permissionsList, List<Map<String, Object>> returnMap) {
+        HashMap<String, Object> firstPageMap = new HashMap<>();
+        ArrayList<Object> firstPageList = new ArrayList<>();
+
+        HashMap<String, Object> projectMap = new HashMap<>();
+        ArrayList<Object> projectList = new ArrayList<>();
+
+        HashMap<String, Object> goodsMap = new HashMap<>();
+        ArrayList<Object> goodsList = new ArrayList<>();
+
+        HashMap<String, Object> pricesMap = new HashMap<>();
+        ArrayList<Object> pricesList = new ArrayList<>();
+
+        HashMap<String, Object> supplierMap = new HashMap<>();
+        ArrayList<Object> supplierList = new ArrayList<>();
+
+        HashMap<String, Object> agentMap = new HashMap<>();
+        ArrayList<Object> agentList = new ArrayList<>();
+
+        HashMap<String, Object> bidsMap = new HashMap<>();
+        ArrayList<Object> bidsList = new ArrayList<>();
+
+        HashMap<String, Object> contractMap = new HashMap<>();
+        ArrayList<Object> contractList = new ArrayList<>();
+
+        HashMap<String, Object> templateMap = new HashMap<>();
+        ArrayList<Object> templateList = new ArrayList<>();
+
+        HashMap<String, Object> systemMap = new HashMap<>();
+        ArrayList<Object> systemList = new ArrayList<>();
+
+
+        //数据处理
+        for (int i = 0; i < permissionsList.size(); i++) {
+            if(1==permissionsList.get(i).getPermId()){
+                firstPageMap.put("permission",permissionsList.get(i));
+            }else if(14==permissionsList.get(i).getPermId()){
+                projectMap.put("permission",permissionsList.get(i));
+            }else if(22==permissionsList.get(i).getPermId()){
+                goodsMap.put("permission",permissionsList.get(i));
+            }else if(31==permissionsList.get(i).getPermId()){
+                pricesMap.put("permission",permissionsList.get(i));
+            }else if(37==permissionsList.get(i).getPermId()){
+                supplierMap.put("permission",permissionsList.get(i));
+            }else if(47==permissionsList.get(i).getPermId()){
+                agentMap.put("permission",permissionsList.get(i));
+            }else if(57==permissionsList.get(i).getPermId()){
+                bidsMap.put("permission",permissionsList.get(i));
+            }else if(61==permissionsList.get(i).getPermId()){
+                contractMap.put("permission",permissionsList.get(i));
+            }else if(65==permissionsList.get(i).getPermId()){
+                templateMap.put("permission",permissionsList.get(i));
+            }else if(81==permissionsList.get(i).getPermId()){
+                systemMap.put("permission",permissionsList.get(i));
+            }
+
+
+
+            if(1==permissionsList.get(i).getParentPermId()){
+                firstPageList.add(permissionsList.get(i));
+                firstPageMap.put("list",firstPageList);
+            }else if(14==permissionsList.get(i).getParentPermId()){
+                projectList.add(permissionsList.get(i));
+                projectMap.put("list",projectList);
+            }else if(22==permissionsList.get(i).getParentPermId()){
+                goodsList.add(permissionsList.get(i));
+                goodsMap.put("list",goodsList);
+            }else if(31==permissionsList.get(i).getParentPermId()){
+                pricesList.add(permissionsList.get(i));
+                pricesMap.put("list",pricesList);
+            }else if(37==permissionsList.get(i).getParentPermId()){
+                supplierList.add(permissionsList.get(i));
+                supplierMap.put("list",supplierList);
+            }else if(47==permissionsList.get(i).getParentPermId()){
+                agentList.add(permissionsList.get(i));
+                agentMap.put("list",agentList);
+            }else if(57==permissionsList.get(i).getParentPermId()){
+                bidsList.add(permissionsList.get(i));
+                bidsMap.put("list",bidsList);
+            }else if(61==permissionsList.get(i).getParentPermId()){
+                contractList.add(permissionsList.get(i));
+                contractMap.put("list",contractList);
+            }else if(65==permissionsList.get(i).getParentPermId()){
+                templateList.add(permissionsList.get(i));
+                templateMap.put("list",templateList);
+            }else if(81==permissionsList.get(i).getParentPermId()){
+                systemList.add(permissionsList.get(i));
+                systemMap.put("list",systemList);
+            }
+        }
+        returnMap.add(systemMap);
+        returnMap.add(templateMap);
+        returnMap.add(contractMap);
+        returnMap.add(bidsMap);
+        returnMap.add(agentMap);
+        returnMap.add(supplierMap);
+        returnMap.add(pricesMap);
+        returnMap.add(goodsMap);
+        returnMap.add(projectMap);
+        returnMap.add(firstPageMap);
+    }
+
+    public List<Permissions> getThirdPermissionBySecondPermissionId(String permissionId) {
+        return permissionRepository.findByPermissionId(Long.parseLong(permissionId));
+    }
+
+    //根据角色id查找角色权限
+    public List<Map<String, Object>> getPermissionByRoleId(long roleId) {
+        List<Permissions> permissionsList = permissionRepository.getPermissionByRoleId(roleId);
+
+        //要返回的结果集
+        List<Map<String,Object>> returnMap = new ArrayList<>();
+
+        proPermissions(permissionsList,returnMap);
+
+        //调用公共的处理权限的方法
+        return returnMap;
+    }
+
+    //根据角色id查询该角色所拥有的权限(三级权限)
+    public List<Permissions> getThirdPermissionByRoleId(String roleId, String secondPermissionId) {
+        //先查询改用户的所有权限
+        List<Permissions> permissionsList = permissionRepository.getPermissionByRoleId(Long.parseLong(roleId));
+        ArrayList<Permissions> returnList = new ArrayList<>();
+
+        for (int i = 0; i < permissionsList.size(); i++) {
+            if(secondPermissionId.equals(permissionsList.get(i).getParentPermId().toString())){
+                returnList.add(permissionsList.get(i));
+            }
+        }
+        return returnList;
+    }
+}
