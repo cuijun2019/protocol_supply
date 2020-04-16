2fa3911559c19c5e02858e1546f50e688422d98e
cuijun <18319670614@139.com>
2020-04-16 16:49:06 +0800

    创建流程表

---
+++ src/main/java/com/etone/protocolsupply/model/entity/flowEngine/FlowTransfer.java
@@ -0,0 +1,64 @@
+package com.etone.protocolsupply.model.entity.flowEngine;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * 流程线
+ */
+@Data
+@Entity
+@Table(name = "FLOW_TRANSFER")
+public class FlowTransfer implements Serializable {
+
+    /**
+     * 流程线id
+     */
+    @Id
+    @Column(name = "FLOW_TRANSFER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long flowTransferId;
+
+    /**
+     * 上一节点编号
+     */
+    @Column(name = "PREV_NODE_CODE", length = 100)
+    private String prevNodeCode;
+
+    /**
+     * 后一节点编号
+     */
+    @Column(name = "NEXT_NODE_CODE", length = 100)
+    private String nextNodeCode;
+
+    /**
+     * 角色
+     */
+    @Column(name = "ROLES", length = 500)
+    private String roles;
+
+    /**
+     * SQL语句
+     */
+    @Column(name = "SQL", length = 1000)
+    private String sql;
+
+    /**
+     * 备注
+     */
+    @Column(name = "REMARK", length = 1000)
+    private String remark;
+
+    /**
+     * 关联流程id
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = {"hibernateLazyInitializer"})
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JoinColumn(name = "FLOW_ID")
+    private FlowInfo flowInfo;
+}
