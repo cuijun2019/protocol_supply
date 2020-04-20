0e620e894224c2a13c8295f3fe4e1eb90f634a51
cuijun <18319670614@139.com>
2020-04-20 19:35:19 +0800

    修改待办已办entity

--- src/main/java/com/etone/protocolsupply/model/entity/procedure/BusiApproveResult.java
+++ src/main/java/com/etone/protocolsupply/model/entity/procedure/BusiApproveResult.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * 已办
+ */
 @Data
 @Entity
 @Table(name = "BUSI_APPROVE_RESULT")
@@ -16,22 +19,53 @@ public class BusiApproveResult implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long approveId;
 
+    /**
+     * 业务表id
+     */
+    @Column(name = "BUSINESS_ID", length = 100)
+    private String businessId;
+
+    /**
+     * 流程节点编号
+     */
+    @Id
+    @Column(name = "FLOW_NODE_CODE", length = 100)
+    private String flowNodeCode;
+
+    /**
+     * 已办类型
+     */
     @Column(name = "APPROVE_TYPE", length = 100)
-    private String approveType; // 已办类型
+    private String approveType;
 
+    /**
+     * 已办主题
+     */
     @Column(name = "APPROVE_SUBJECT", length = 100)
-    private String approveSubject; // 已办主题
+    private String approveSubject;
 
-    @Column(name = "APPROVESTATE", length = 100)
-    private String approveState; // 当前状态
+    /**
+     * 当前状态
+     */
+    @Column(name = "APPROVE_STATE", length = 100)
+    private String approveState;
 
-    @Column(name = "PARENTACTOR", length = 100)
-    private String parentActor; // 当前处理人
+    /**
+     * 当前处理人
+     */
+    @Column(name = "PARENT_ACTOR", length = 100)
+    private String parentActor;
 
-    @Column(name = "APPROVEINITOR_ID", length = 100)
-    private String approveInitorId; // 创建人ID
+    /**
+     * 创建人ID
+     */
+    @Column(name = "APPROVE_INITOR_ID", length = 100)
+    private String approveInitorId;
 
-    @Column(name = "APPROVE_STARTTIME")
+    /**
+     * 创建时间
+     */
+    @Column(name = "APPROVE_START_TIME")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date approveStartTime; // 创建时间
+    private Date approveStartTime;
 }
--- src/main/java/com/etone/protocolsupply/model/entity/procedure/BusiJbpmFlow.java
+++ src/main/java/com/etone/protocolsupply/model/entity/procedure/BusiJbpmFlow.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * 待办
+ */
 @Data
 @Entity
 @Table(name = "BUSI_JBPM_FLOW")
@@ -16,26 +19,53 @@ public class BusiJbpmFlow implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 业务表id
+     */
+    @Column(name = "BUSINESS_ID", length = 100)
+    private String businessId;
+
+    /**
+     * 流程节点编号
+     */
+    @Id
+    @Column(name = "FLOW_NODE_CODE", length = 100)
+    private String flowNodeCode;
+
+    /**
+     * 待办类型
+     */
     @Column(name = "BUSINESS_TYPE", length = 100)
-    private String businessType; // 待办类型
+    private String businessType;
 
+    /**
+     * 待办主题（业务表名称）
+     */
     @Column(name = "BUSINESS_SUBJECT", length = 100)
-    private String businessSubject; // 待办主题
+    private String businessSubject;
 
-    @Column(name = "TASKSTATE", length = 100)
-    private String taskState; // 当前状态
+    /**
+     * 当前状态
+     */
+    @Column(name = "TASK_STATE", length = 100)
+    private String taskState;
 
-    @Column(name = "PARENTACTOR", length = 100)
-    private String parentActor; // 当前处理人
+    /**
+     * 当前处理人
+     */
+    @Column(name = "PARENT_ACTOR", length = 100)
+    private String parentActor;
 
-    @Column(name = "FLOWINITOR_ID", length = 100)
-    private String flowInitorId; // 创建人ID
+    /**
+     * 创建人ID
+     */
+    @Column(name = "FLOW_INITOR_ID", length = 100)
+    private String flowInitorId;
 
-    @Column(name = "FLOW_STARTTIME")
+    /**
+     * 创建时间
+     */
+    @Column(name = "FLOW_START_TIME")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date flowStartTime; // 创建时间
-
-    @Column(name = "BUSINESS_ID", length = 20)
-    private Long businessId; // 代办id（项目、产品id）
-
+    private Date flowStartTime;
 }
